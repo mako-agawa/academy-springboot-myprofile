abd9f6dd4dc499c3115297ed7913ac85717b390f
Makoto_Agawa <51506765+mako-agawa@users.noreply.github.com>
2025-08-09 14:21:45 +0900

    propertiesファイル修正

--- src/test/java/com/spring/springbootapplication/SpringbootApplicationTests.java
+++ src/test/java/com/spring/springbootapplication/SpringbootApplicationTests.java
@@ -1,9 +1,12 @@
 package com.spring.springbootapplication;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@MapperScan("com.example.demo.dao") // 実際のパッケージに変更
+@SpringBootApplication
 class SpringbootApplicationTests {
 
 	@Test
